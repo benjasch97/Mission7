0e914f47a8d7d78c17a175d986e7f3657b6db59d
benjasch97 <schneider.benjamina97@gmail.com>
2022-03-16 20:14:30 -0600

    mission11

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Amazon.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,6 +36,12 @@ namespace Amazon
                 options.UseSqlite(Configuration["ConnectionStrings:AmazonDBConnection"]);
             });
 
+            services.AddDbContext<AppIdentityDBContext>(options =>
+                options.UseSqlite(Configuration["ConnectionStrings:IdentityConnection"]));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDBContext>();
+
             services.AddScoped<IAmazonRepository, EFAmazonRepository>();
             services.AddScoped<IPurchaseRepository, EFPurchaseRepository>();
 
@@ -62,6 +69,9 @@ namespace Amazon
             app.UseSession();
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute("categorypage",
@@ -83,6 +93,9 @@ namespace Amazon
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
             });
+
+            IdentitySeedData.EnsurePopulated(app);
+
         }
     }
 }
